05493c972ebabed910f85eab326042cdcf551a51
Saurav Bhise <sauravbhise2002@gmail.com>
2022-10-20 17:39:58 +0530

    Cleanup & Add functionality

--- Linegraph.js
+++ Linegraph.js
@@ -1,77 +1,93 @@
-function graph()
-{
-    var xValues=[];
-    var yValues=[];
-    if(document.getElementById(1).checked){
-        var duration="weekly";
-    }
-    else if(document.getElementById(2).checked){
-        var duration="monthly";
-    }
-    else if(document.getElementById(3).checked){
-        var duration="quarterly";
-    }
-    else if(document.getElementById(4).checked){
-        var duration="yearly";
-    }
-var body=JSON.stringify({
-    "currency1":"USD",
-    "currency2":document.getElementById("scurr").value,
-    "chartType":duration,
-    "year":document.getElementById("year").value
-});
+let myChart;
 
-var myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
+function draw(xValues, yValues, highest) {
+  console.log(xValues);
+  console.log(yValues);
+  myChart = new Chart("myChart", {
+    type: "line",
+    data: {
+      labels: xValues,
+      datasets: [
+        {
+          fill: false,
+          lineTension: 0,
+          backgroundColor: "rgba(0,0,255,1.0)",
+          borderColor: "rgba(0,0,255,0.1)",
+          data: yValues,
+        },
+      ],
+    },
+    options: {
+      legend: { display: false },
+      scales: {
+        yAxes: [{ ticks: { min: yValues.sort()[0], max: highest["value"] } }],
+      },
+    },
+  });
+}
 
-var requestOptions = {
-  method: 'POST',
-  headers: myHeaders,
-  body: body,
-  redirect: 'follow'
-};
+async function graph() {
+  let xValues = [];
+  let yValues = [];
+  let duration;
+  if (document.getElementById(1).checked) {
+    duration = "weekly";
+  } else if (document.getElementById(2).checked) {
+    duration = "monthly";
+  } else if (document.getElementById(3).checked) {
+    duration = "quarterly";
+  } else if (document.getElementById(4).checked) {
+    duration = "yearly";
+  }
 
-fetch("https://backend-mitwpu-team7.vercel.app/chart", requestOptions)
-  .then(response => response.text())
-  .then(result=>{
-    
-    result = JSON.parse(result);
-    var data=result["datapoints"]
-    data.forEach(element => {
+  let body = JSON.stringify({
+    currency1: "USD",
+    currency2: document.getElementById("scurr").value,
+    chartType: duration,
+    year: document.getElementById("year").value,
+  });
 
-            xValues.push(element["date"])
-            yValues.push(element["value"])   
-    })
-})
-  .catch(error => console.log('error', error));
+  let myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  let requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: body,
+    redirect: "follow",
+  };
+
+  let highest;
+
+  try {
+    const response = await fetch(
+      "https://backend-mitwpu-team7.vercel.app/chart",
+      requestOptions
+    );
+
+    const result = await response.json();
+
+    highest = result["highest"];
+    let data = result["datapoints"];
+
+    data.forEach((element) => {
+      if (element["value"] !== 0) {
+        xValues.push(element["date"]);
+        yValues.push(element["value"]);
+      }
+    });
+  } catch (error) {
+    console.log("error", error);
+  }
+
   console.log(xValues);
   console.log(yValues);
 
-    var myChart=new Chart("myChart", {
-        type: "line",
-        data: {
-          labels: xValues,
-          datasets: [{
-            fill: false,
-            lineTension: 0,
-            backgroundColor: "rgba(0,0,255,1.0)",
-            borderColor: "rgba(0,0,255,0.1)",
-            data: yValues
-          }]
-        },
-        options: {
-          legend: {display: false},
-          scales: {
-            yAxes: [{ticks: {min: 0, max:100}}],
-          }
-        }
-      });
+  if (myChart != undefined) {
+    myChart.clear();
+  }
+  draw(xValues, yValues, highest);
 
-        var max=Math.max.apply(Math, yValues);
-        document.getElementById("hv").innerHTML="Max value="+max;
-
-        var min=Math.min.apply(Math, yValues);
-        document.getElementById("lv").innerHTML="Min value="+min;
-
-    }
-
+  document.getElementById("hv").innerHTML = "Max value=" + highest["value"];
+  document.getElementById("lv").innerHTML = "Min value=" + yValues.sort()[0];
+}
--- index.js
+++ index.js
@@ -1,28 +1,37 @@
-    import {currencyCodes} from "./currency.js";
-    var codes=Object.keys(currencyCodes)
+import { currencyCodes } from "./currency.js";
+var codes = Object.keys(currencyCodes);
 
-    var cc=[];
-    for (var i = 0; i < codes.length; i++) {
-        cc.push(codes[i]);
+var cc = [];
+for (var i = 0; i < codes.length; i++) {
+  cc.push(codes[i]);
+}
+var select = document.getElementById("scurr");
+for (var i = 0; i < cc.length; i++) {
+  var optn = cc[i];
+  var el = document.createElement("option");
+  el.textContent = optn;
+  el.value = optn;
+  select.appendChild(el);
+}
 
-    }
-    var select=document.getElementById("scurr");
-    for (var i = 0; i < cc.length; i++) {
-        var optn = cc[i];
-        var el = document.createElement("option");
-        el.textContent = optn;
-        el.value = optn;
-        select.appendChild(el);
-    }
-
-    var year=document.getElementById("year");
-    var yr=["2012","2013","2014","2015","2016","2017","2018","2019","2020","2021","2022"];
-    for (var i = 0; i < yr.length; i++) {
-        var optn = yr[i];
-        var el = document.createElement("option");
-        el.textContent = optn;
-        el.value = optn;
-        year.appendChild(el);
-    }
-
-
+var year = document.getElementById("year");
+var yr = [
+  "2012",
+  "2013",
+  "2014",
+  "2015",
+  "2016",
+  "2017",
+  "2018",
+  "2019",
+  "2020",
+  "2021",
+  "2022",
+];
+for (var i = 0; i < yr.length; i++) {
+  var optn = yr[i];
+  var el = document.createElement("option");
+  el.textContent = optn;
+  el.value = optn;
+  year.appendChild(el);
+}
